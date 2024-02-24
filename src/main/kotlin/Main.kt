9fd6810af00392bd13e0922af7db9ddc777c1065
jjegarcia <jjegarcia@hotmail.com>
2024-02-24 14:56:23 +0000

    added sealed fizzBuzz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+import java.util.*
+
+const val FIZZ = "Fizz"
+const val BUZZ = "Buzz"
+
+sealed class Message(val divider: Int, val content: String) {
+    object Fizz : Message(3, FIZZ)
+    object Buzz : Message(5, BUZZ)
+//    object FizzBuzz : Message()
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
@@ -19,7 +30,7 @@ fun findLongestUnique(str: String): String {
             bundle += char
             if (bundle.length == k + 1) {
                 val lastIndex = getLastIndex(newCandidate, bundle[0])
-                newCandidate = newCandidate.subSequence(lastIndex + 1, newCandidate.length ) as String
+                newCandidate = newCandidate.subSequence(lastIndex + 1, newCandidate.length) as String
                 bundle = bundle.subSequence(1, bundle.length - 1) as String
                 bundle += char
             }
@@ -61,4 +72,70 @@ fun SeatingStudents(arr: Array<Int>): Int {
     }
     return count
 }
+
+fun fizzBuzz(n: Int): Unit {
+    // Write your code here
+    for (i in 1..n) {
+        println(determineOut(i))
+    }
+}
+
+private fun determineOut(i: Int): String {
+    val isFizz = determineMatch(i, Message.Fizz)
+    val isBuzz = determineMatch(i, Message.Buzz)
+    return when {
+        isFizz && isBuzz -> "${Message.Fizz.content}${Message.Buzz.content}"
+        isFizz -> Message.Fizz.content
+        isBuzz -> Message.Buzz.content
+        else -> i.toString()
+    }
+}
+
+private fun determineMatch(i: Int, message: Message) = i.rem(message.divider) == 0
+
+fun capitaliser(message: String): String {
+    var test = ""
+    val parsed = message.split(" ").map { word ->
+        word.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+    }
+    parsed.forEachIndexed { index, word ->
+        if (index < parsed.size - 1) test += word + " "
+        else test += word
+    }
+    return test
+}
+
+fun printTimeArray(array: Array<Time>) {
+    array.forEachIndexed { index, time ->
+        println("${index + 1}) ${time.string}")
+    }
+}
+
+fun createTimeArray(inputArray: Array<String>): Array<Time> {
+    return inputArray.map { str ->
+        Time(str, createTimeItem(item = str))
+    }.sortedBy { time ->
+        time.minutes
+    }.toTypedArray()
+}
+
+fun createTimeItem(item: String): Int {
+    val test = item.split(":")
+    return if (test[1].contains("pm")) {
+        (test[0].toInt() + 12) * 60 + test[1].replace("pm", "").toInt()
+    } else {
+        test[0].toInt() + test[1].replace("am", "").toInt()
+    }
+}
+
+fun factorial(n: Int): Int {
+    return if (n > 1) {
+        n * factorial(n - 1)
+    } else n
+}
+
+data class Time(
+    val string: String,
+    val minutes: Int
+)
 
